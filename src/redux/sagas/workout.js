aded20ede0ee0d02970d2b32529af139b5a459e5
mes32 <stockman.mike@gmail.com>
2019-04-17 14:26:32 -0500

    Add a saga for workouts.

---
+++ src/redux/sagas/workout.js
@@ -0,0 +1,28 @@
+import { put, takeLatest } from 'react-saga/effects';
+import axios from 'axios';
+
+function* fetchWorkouts() {
+    try {
+        // yield axios.get('api/workout');
+    } catch (error) {
+        console.log('Error in FETCH_WORKOUTS:', error);
+        alert('Unable to fetch workouts from server.')
+    }
+}
+
+function* addWorkout(action) {
+    try {
+        yield axios.post('api/workout', action.payload);
+        yield put({ type: 'FETCH_WORKOUTS' });
+    } catch (error) {
+        console.log('Error in ADD_WORKOUTS:', error);
+        alert('Unable to submit workout to server.')
+    }
+}
+
+function* workoutSaga() {
+    yield takeLatest('FETCH_WORKOUTS', fetchWorkouts);
+    yield takeLatest('ADD_WORKOUT', addWorkout);
+}
+
+export default workoutSaga;
