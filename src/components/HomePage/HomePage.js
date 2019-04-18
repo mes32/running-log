536f8d0d7703469678cb63c32af1243924328376
mes32 <stockman.mike@gmail.com>
2019-04-18 09:31:24 -0500

    Move the workout form on HomePage into a new component WorkoutForm.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,55 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import WorkoutForm from '../WorkoutForm/WorkoutForm';
+
 class HomePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            distance: '',
-            duration: '',
-            incline: ''
-        };
-    }
-
-    handleChange = (event) => {
-        this.setState({
-            ...this.state,
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        if (this.state.distance) {
-            const action = {
-                type: 'ADD_WORKOUT',
-                payload: {
-                    distance_miles: this.convertInt(this.state.distance),
-                    duration_minutes: this.convertInt(this.state.duration),
-                    incline_percent: this.convertInt(this.state.incline)
-                }
-            };
-            this.props.dispatch(action);
-        }
-    }
-
-    convertInt = (string) => {
-        if (string === '') {
-            return null;
-        } else {
-            return parseInt(string);
-        }
-    }
-
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input name="distance" onChange={this.handleChange} type="number" placeholder="Distance (miles)" />
-                    <input name="duration" onChange={this.handleChange} type="number" placeholder="Time (minutes)" />
-                    <input name="incline" onChange={this.handleChange} type="number" placeholder="Incline (percent)" />
-                    <input type="submit" value="Submit" />
-                </form>
+                <WorkoutForm />
             </div>
         );
     }
