09b5ecf913beb766dbef66b5bbebbd822987b7b2
mes32 <stockman.mike@gmail.com>
2019-04-26 23:57:41 -0500

    Add a settings page.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import { connect } from 'react-redux';
 
 import HomePage from '../HomePage/HomePage';
+import SettingsPage from '../SettingsPage/SettingsPage';
 
 import './App.css';
 
@@ -16,6 +17,10 @@ class App extends Component {
                         <Route
                             exact path="/home"
                             component={HomePage}
+                        />
+                        <Route
+                            exact path="/settings"
+                            component={SettingsPage}
                         />
                     </Switch>
                 </div>
---
+++ src/components/SettingsPage/SettingsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SettingsPage(props) {
+    return (
+        <div>
+            <p>[ Settings Page ]</p>
+        </div>
+    );
+}
+
+export default SettingsPage;
